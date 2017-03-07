e2f622e1f69a706f6ba993025f1e489f92b43581
LCDW16 <LCDobWang16@gmail.com>
2017-03-07 21:45:03 +0100

    Add files via upload

--- plot3.R
+++ plot3.R
@@ -21,14 +21,12 @@ data1[,8] <- as.numeric(data1[,8])
 data1[,9] <- as.numeric(data1[,9])
 
 ## plot datetime and energy sub metering
+png("plot3.png", width=480, height=480)
+
 plot(datetime, data1$Sub_metering_1,type = "l", xlab="", ylab = "Energy sub metering")
 lines(datetime, data1$Sub_metering_2, type="l", col="red")
 lines(datetime, data1$Sub_metering_3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
-
-library(datasets)
-## copy plot to PNG file
-dev.copy(png, width = 480, height = 480, file = "plot3.png")
 
 ## close PNG device
 dev.off()
